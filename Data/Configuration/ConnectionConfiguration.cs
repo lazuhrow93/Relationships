ae7d29c8a777f3ed8acb21d652d3ed020da0290e
lazuhrow93 <lazuhrow93@gmail.com>
2025-04-06 18:55:09 -0500

    adding the configuration for the tables. And foreign keys for connections/characters

--- Data/Configuration/ConnectionConfiguration.cs
+++ Data/Configuration/ConnectionConfiguration.cs
@@ -8,6 +8,16 @@ public class ConnectionConfiguration : IEntityTypeConfiguration<Connection>
 {
     public void Configure(EntityTypeBuilder<Connection> builder)
     {
+        builder
+            .HasOne(e => e.CharacterStart)
+            .WithMany()
+            .HasForeignKey(e => e.CharacterStartId)
+            .OnDelete(DeleteBehavior.Restrict);
 
+        builder
+            .HasOne(e => e.CharacterEnd)
+            .WithMany()
+            .HasForeignKey(e => e.CharacterEndId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
---
+++ Data/Migrations/20250406235221_InitialCreate.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "character",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "connection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharacterStartId = table.Column<int>(type: "int", nullable: false),
+                    CharacterEndId = table.Column<int>(type: "int", nullable: false),
+                    ConnectionType = table.Column<int>(type: "int", nullable: false),
+                    CharacterId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_connection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_connection_character_CharacterEndId",
+                        column: x => x.CharacterEndId,
+                        principalTable: "character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_connection_character_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "character",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_connection_character_CharacterStartId",
+                        column: x => x.CharacterStartId,
+                        principalTable: "character",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_connection_CharacterEndId",
+                table: "connection",
+                column: "CharacterEndId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_connection_CharacterId",
+                table: "connection",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_connection_CharacterStartId",
+                table: "connection",
+                column: "CharacterStartId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "connection");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "character");
+        }
+    }
+}
