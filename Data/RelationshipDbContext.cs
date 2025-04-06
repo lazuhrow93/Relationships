ab37abc075ff370a4261ca2da0c19e3254c3f812
lazuhrow93 <lazuhrow93@gmail.com>
2025-04-06 17:59:14 -0500

    added  configuration for entity type configurations

--- Data/RelationshipDbContext.cs
+++ Data/RelationshipDbContext.cs
@@ -4,5 +4,38 @@ namespace Data;
 
 public class RelationshipDbContext : DbContext
 {
+    public RelationshipDbContext(DbContextOptions<RelationshipDbContext> options)
+    : base(options)
+    {
+    }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RelationshipDbContext).Assembly);
+
+        // Convert all table names to snake_case
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            // Convert table name
+            var tableName = entity.GetTableName();
+            if (tableName != null)
+            {
+                entity.SetTableName(ToSnakeCase(tableName));
+            }
+        }
+    }
+
+    #region Private Helpers
+
+    private static string ToSnakeCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return input;
+
+        return string.Concat(
+            input.Select((c, i) =>
+                i > 0 && char.IsUpper(c) ? "_" + char.ToLower(c) : char.ToLower(c).ToString())
+        );
+    }
+
+    #endregion
 }
--- Data/Setup.cs
+++ Data/Setup.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Data;
 
 public static class Setup
 {
-    public static IServiceCollection AddDatabase(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddDatabase(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
         var assembly = typeof(Setup).Assembly;
 
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string"
+            + "'DefaultConnection' not found.");
+
+        return serviceCollection.AddDbContext<RelationshipDbContext>(opt => opt.UseSqlServer(connectionString));
     }
 }
